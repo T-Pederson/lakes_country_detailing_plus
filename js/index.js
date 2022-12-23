3b3563f32b7466d5abce377d3685e1260f7d5521
Tyson <Tysonpederson95@gmail.com>
2022-12-22 22:02:05 -0600

    Fix display issue on tablet related to grid-template-rows

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+body = document.querySelector("body");
 menu = document.querySelector(".menu");
 mobileDropdown = document.querySelector(".mobile_dropdown");
 mobileServices = document.querySelector(".mobile_services");
@@ -9,8 +10,10 @@ mobileServices.addEventListener("click", toggle_services_dropdown);
 function toggle_mobile_navbar() {
   if (getComputedStyle(mobileDropdown).display == "none") {
     mobileDropdown.style.display = "flex";
+    body.style.gridTemplateRows = "60px auto auto 1fr 80px";
   } else {
     mobileDropdown.style.display = "none";
+    body.style.gridTemplateRows = "60px auto 1fr 80px";
   }
 }
 
